2375f344b11877c0d2d28b1a19305abb9e3c216c
Sam Partington <sam@whiteoctober.co.uk>
2016-07-06 15:09:02 +0100

    Add forms library and example route

--- handlers.py
+++ handlers.py
@@ -1,10 +1,37 @@
 import webapp2
-import templates as templates
+import templates
+import forms
+import config
 
 class BaseHandler(webapp2.RequestHandler):
-    def render(self, template, variables):
+    def render(self, template, variables={}):
         templates.output_page(self, templates.render_page(self, template, variables))
 
 class MainHandler(BaseHandler):
     def get(self, args):
         self.render('index', {'message': 'Hello, world!'})
+
+class FormHandler(BaseHandler):
+    def get(self):
+        # render the form using the form fields dictionary built using the forms.build_form_fields(form, variables) function
+        # variables are an empty dictionary as the form isn't filled in yet
+        self.render("form", {
+            "form": forms.build_form_fields("example", {})
+        })
+
+    def post(self):
+        form_name = "example"
+        # create an array to collect form field errors in
+        # if the length of the array is zero, output the form again
+        variable_set, errors = forms.build_variable_set(self, form_name)
+        if len(errors) > 0:
+            # render the form using the form fields dictionary built using the forms.build_form_fields(form, variables) function
+            # variables are the variable_set dictionary from above
+            self.render("form", {
+                "form": forms.build_form_fields(form_name, variable_set),
+                "errors": errors,
+                "error_conditions": config.error_conditions
+            })
+        else:
+            # no errors, show the thanks page
+            self.render('thanks', {'variable_set': variable_set})

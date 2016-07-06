5df33ae7d6bdd6547b0df60a97bd1ebebdb02b4a
Sam Partington <sam@whiteoctober.co.uk>
2016-07-06 12:49:33 +0100

    Use templating for sample page

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 import webapp2
+import templates as templates
 
 class MainHandler(webapp2.RequestHandler):
     def get(self, args):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        templates.render(self, 'index', {'message': 'Hello, world!'})
---
+++ templates.py
@@ -0,0 +1,28 @@
+import jinja2
+import os
+
+### TEMPLATING FUNCTIONS ###
+
+# set the Jinja environment using autoescaping of html and using the file system loader for templates
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader('templates'),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+# super simple template renderer
+
+def render_page(self, template, variables):
+    # builds the path to the template
+    if not ".html" in template:
+        template += ".html"
+    # loads it into the Jinja environment
+    template = JINJA_ENVIRONMENT.get_template(template)
+    # outputs the HTML from the rendered template and variables
+    return template.render(variables)
+
+def output_page(self, html):
+    # outputs the HTML from the rendered template and variables
+    self.response.out.write(html)
+
+def render(self, template, variables):
+    output_page(self, render_page(self, template, variables))

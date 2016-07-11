d502488d8d2d7899beff340705e14cb76d124ddf
Sam Partington <sam@whiteoctober.co.uk>
2016-07-11 15:57:18 +0100

    Move to extended route syntax in readiness for adding subdomain routes

--- routes.py
+++ routes.py
@@ -1,10 +1,10 @@
 import handlers as handlers
 
 ROUTES = [
-    ('/sample-form', handlers.FormHandler),
-    ('/restricted-inline', handlers.RestrictedAreaHandler),
-    ('/restricted', handlers.RestrictedByDecoratorHandler),
-    ('/restricted-app-engine', handlers.RestrictedByAppEngineDecoratorHandler),
-    ('/restricted-role', handlers.RestrictedByRoleDecoratorHandler),
-    ('/', handlers.HomeHandler),
+    webapp2.Route('/sample-form', handler=handlers.FormHandler, name="form"),
+    webapp2.Route('/restricted-inline', handler=handlers.RestrictedAreaHandler, name="restricted-inline"),
+    webapp2.Route('/restricted', handler=handlers.RestrictedByDecoratorHandler, name="restricted"),
+    webapp2.Route('/restricted-app-engine', handler=handlers.RestrictedByAppEngineDecoratorHandler, name="restricted-app-engine"),
+    webapp2.Route('/restricted-role', handler=handlers.RestrictedByRoleDecoratorHandler, name="restricted-role"),
+    webapp2.Route('/', handler=handlers.HomeHandler, name="home"),
 ]
